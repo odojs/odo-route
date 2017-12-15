9edd1c6a0eb9104a9ab805fc1044c090a28dbe2e
Thomas Coats <github@voodoolabs.net>
2017-12-15 22:53:04 +1300

    nicer api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const pathtoregexp = require('path-to-regexp')
 
 const route = () => {
-  let _routes = []
+  let routes = []
   const res = (pattern, callback, options) => {
     if (callback != null) {
       if (pattern === '*') pattern = '(.*)'
@@ -10,17 +10,17 @@ const route = () => {
         match: pathtoregexp(pattern, options),
         cb: callback
       }
-      _routes.push(entry)
+      routes.push(entry)
       return {
         off: () => {
-          const index = _routes.indexOf(entry)
-          if (index != -1) _routes.splice(index, 1)
+          const index = routes.indexOf(entry)
+          if (index != -1) routes.splice(index, 1)
         }
       }
     }
     const qindex = pattern.indexOf('?')
     if (qindex !== -1) pattern = pattern.slice(0, qindex)
-    for (let r of _routes) {
+    for (let r of routes) {
       const result = r.match.exec(decodeURIComponent(pattern))
       if (result == null) continue
       const params = {}
@@ -36,29 +36,22 @@ const route = () => {
     }
     throw new Error('no route found')
   }
-  res.routes = () => _routes
+  res.routes = () => routes
   res.clearAll = () => {
-    _routes = []
+    routes = []
   }
   return res
 }
 
-let globalroute = null
+let _route = null
+const assert = () => {
+  if (_route == null) _route = route()
+  return _route
+}
 
-const globalapi = (pattern, callback, options) => {
+module.exports = (pattern, callback, options) => {
   if (pattern == null) return route()
-  if (globalroute == null) globalroute = route()
-  return globalroute(pattern, callback, options)
+  return assert()(pattern, callback, options)
 }
-
-globalapi.routes = () => {
-  if (globalroute == null) globalroute = route()
-  return globalroute.routes()
-}
-
-globalapi.clearAll = () => {
-  if (globalroute == null) globalroute = route()
-  globalroute.clearAll()
-}
-
-module.exports = globalapi
+module.exports.routes = () => assert().routes()
+module.exports.clearAll = () => assert().clearAll()

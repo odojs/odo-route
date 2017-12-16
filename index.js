9e629866c176c10cab0f50d3740af77984ace034
Thomas Coats <github@voodoolabs.net>
2017-12-16 21:37:56 +1300

    exec powers

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const pathtoregexp = require('path-to-regexp')
 
+const hostname = window.location.hostname
+const hostnameparts = hostname.split('.')
+let subdomain = null
+if (hostnameparts.length == 3)
+  subdomain = hostnameparts[0]
+
 const route = () => {
   let routes = []
   const res = (pattern, callback, options) => {
-    if (callback != null) {
-      if (pattern === '*') pattern = '(.*)'
-      const entry = {
-        pattern: pattern,
-        match: pathtoregexp(pattern, options),
-        cb: callback
-      }
-      routes.push(entry)
-      return {
-        off: () => {
-          const index = routes.indexOf(entry)
-          if (index != -1) routes.splice(index, 1)
-        }
+    if (pattern === '*') pattern = '(.*)'
+    const entry = {
+      pattern: pattern,
+      match: pathtoregexp(pattern, options),
+      cb: callback
+    }
+    routes.push(entry)
+    return {
+      off: () => {
+        const index = routes.indexOf(entry)
+        if (index != -1) routes.splice(index, 1)
       }
     }
-    const qindex = pattern.indexOf('?')
-    if (qindex !== -1) pattern = pattern.slice(0, qindex)
+  }
+  res.exec = (url, next) => {
+    const qindex = url.indexOf('?')
+    let querystring = null
+    if (qindex !== -1) {
+      querystring = url.slice(qindex + 1)
+      url = url.slice(0, qindex)
+    }
     for (let r of routes) {
-      const result = r.match.exec(decodeURIComponent(pattern))
+      const result = r.match.exec(decodeURIComponent(url))
       if (result == null) continue
       const params = {}
       r.match.keys.forEach((key, i) => {
@@ -30,11 +40,15 @@ const route = () => {
         params[key.name] = val
       })
       return r.cb({
-        url: pattern,
-        params: params
+        hostname: hostname,
+        subdomain: subdomain,
+        url: url,
+        params: params,
+        querystring: querystring
       })
     }
-    throw new Error('no route found')
+    if (next == null) throw new Error('no route found')
+    next()
   }
   res.routes = () => routes
   res.clearAll = () => {
@@ -53,5 +67,6 @@ module.exports = (pattern, callback, options) => {
   if (pattern == null) return route()
   return assert()(pattern, callback, options)
 }
+module.exports.exec = (url, next) => assert().exec(url, next)
 module.exports.routes = () => assert().routes()
 module.exports.clearAll = () => assert().clearAll()
